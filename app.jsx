2e2c26686a8c0186b589032ec7b21a8b8d048479
Eirik Brandtzæg <eirikb@eirikb.no>
2019-07-11 22:06:26 +0200

    Test and deploy

--- app.jsx
+++ app.jsx
@@ -1,7 +1,20 @@
+import '@babel/polyfill';
 import domdom from '@eirikb/domdom';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+// import map from './map.json';
+
+
+// console.log(map);
 
 const dd = domdom();
 
-const view = () => <div>:)</div>;
+const view = () => <div id="map">:)</div>;
 
 document.body.appendChild(dd.render(view));
+
+
+var map = L.map('map').setView([60.14, 10.25], 11);
+L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
+  attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
+});
