edfb171ba49ff8c5af6be565a3e02a44baedf438
Eirik Brandtzæg <eirikb@eirikb.no>
2019-07-19 23:37:58 +0200

    Re-add the original mappings, redirects are nice to have

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const proxy = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/api/', proxy({
-  target: 'https://www.stikkut.no/',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api/': ''
-  }
-}));
+['user', 'stikkut', 'app', 'turar'].forEach(path =>
+  app.use(`/${path}`, proxy({
+    target: `https://www.stikkut.no/`,
+    changeOrigin: true,
+  }))
+);
 
 const bundler = new Bundler('index.html');
 app.use(bundler.middleware());
