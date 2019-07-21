d1be231f30cd9c6ff5e831c35156d826458b7f0d
Eirik Brandtzæg <eirikb@eirikb.no>
2019-07-21 18:00:36 +0200

    Test: Application Insights

--- src/app.jsx
+++ src/app.jsx
@@ -5,10 +5,16 @@ import user from './data/user';
 import map from './data/map';
 import Map from './map.jsx';
 import Login from './login.jsx';
+import {ApplicationInsights} from '@microsoft/applicationinsights-web';
 
 const dd = domdom();
 user(dd);
 map(dd);
+
+const appInsights = new ApplicationInsights({
+  config: {instrumentationKey: '4c55265c-dadc-4ad5-9334-b615c3c9e36dRE'}
+});
+appInsights.loadAppInsights();
 
 const view = ({on, when}) => <main>
   {on('info', info => info)}
