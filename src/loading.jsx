fcd12d3ed69757176ab73e2df8c28b48d17e992a
Eirik Brandtzæg <eirikb@eirikb.no>
2019-08-08 23:51:25 +0200

    Loading er nå mindre

--- src/loading.jsx
+++ src/loading.jsx
@@ -1,7 +1,7 @@
 import naruto from '../assets/naruto.gif';
 
 export default function ({ loading }) {
-  return <div class="full loading">
+  return <div class="loading">
     <img src={naruto} alt=""/>
     <h2>{loading}...</h2>
   </div>;
