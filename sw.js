e656211c65ff934ceeddce215441c3d5acfbf629
Eirik Brandtzæg <eirikb@eirikb.no>
2019-08-12 22:43:23 +0200

    Cache map data!

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-console.log(1);
+const cacheName = 'stfu-1';
+
+const urlsToCache = [
+  '/',
+  'https://opencache.statkart.no/gatekeeper/gk/',
+];
+
+self.addEventListener('install', event =>
+  event.waitUntil(
+    caches.open(cacheName).then(cache =>
+      cache.addAll(urlsToCache)
+    )
+  )
+);
+
+self.addEventListener('fetch', event =>
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+            const responseToCache = response.clone();
+            caches.open(cacheName).then(cache =>
+              cache.put(event.request, responseToCache)
+            );
+
+            return response;
+          }
+        );
+      })
+  )
+);
