83aabf08c53b25ade1487badc2eb1219f8cd5ace
Eirik Brandtzæg <eirikb@eirikb.no>
2019-08-13 23:40:23 +0200

    Kalkuler total lengde til mål via inntegnet rute
    reduce reduce reduce reduce

--- src/data/distance.js
+++ src/data/distance.js
@@ -16,4 +16,38 @@ export default ({ on, get, set }) => {
     if (!marker || !gps) return;
     set('pos.distance', marker.distanceTo(gps));
   });
+
+  function pointClosestTo(track, target) {
+    return track.reduce((res, point) =>
+      (!res || point.distanceTo(target) < res.distanceTo(target)) ? point : res
+    );
+  }
+
+  function trackDistance(track, startPoint) {
+    return track.reduce((res, point) => {
+      return { distance: res.distance + res.point.distanceTo(point), point };
+    }, { distance: 0, point: startPoint }).distance;
+  }
+
+  function shortesPath() {
+    const track = (get('track') || []).map(([lon, lat]) => new L.LatLng(lat, lon));
+    const marker = toLatLng('pos.marker');
+    const gps = toLatLng('pos.gps');
+    if (!track || !gps || !marker) return;
+
+    const pointClosestToMarker = pointClosestTo(track, marker);
+    const pointClosestToGps = pointClosestTo(track, gps);
+
+    const pointClosestToGpsIndex = track.indexOf(pointClosestToGps);
+    const pointClosestToMarkerIndex = track.indexOf(pointClosestToMarker);
+    const startIndex = Math.min(pointClosestToGpsIndex, pointClosestToMarkerIndex);
+    const endIndex = Math.max(pointClosestToGpsIndex, pointClosestToMarkerIndex);
+
+    const leftPath = trackDistance(track.slice(startIndex, endIndex + 1), gps);
+    const rightPath = trackDistance(track.slice(endIndex, track.length).concat(track.slice(0, startIndex + 1)), gps);
+    set('pos.trackDistance', Math.min(leftPath, rightPath));
+  }
+
+  on('!+* pos.{gps,marker}', shortesPath);
+  on('!+* track', shortesPath);
 };
--- src/distance.jsx
+++ src/distance.jsx
@@ -1,5 +1,7 @@
 export default function ({ on }) {
   return <div class="distance">
     {on('pos.distance', distance => `Kråkedistanse: ${Math.floor(distance)}m`)}
+    <br/>
+    {on('pos.trackDistance', distance => `Stidistanse: ${Math.floor(distance)}m`)}
   </div>;
 }
