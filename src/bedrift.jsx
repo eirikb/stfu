dc54fa1923d93290de1072b506ec7dfe09cf872b
hanspf <hpfarstad@gmail.com>
2020-05-02 21:54:04 +0200

    styling ranking table

--- src/bedrift.jsx
+++ src/bedrift.jsx
@@ -1,12 +1,12 @@
 export default function ({ when, get, set }) {
   return <div class="modal">
     <a class="modal-close fa fa-2x fa-times" onClick={() => set('route', 'home')}/>
-    <h1>Bedrift - toppliste</h1>
+    <h1>Toppliste min bedrift</h1>
 
     {when('bedrift.loading', [
       true, () => <span>Laster...</span>,
       false, () => <div>
-        <table>
+        <table class="ranking">
           <thead>
           <tr>
             <th>Nr.</th>
